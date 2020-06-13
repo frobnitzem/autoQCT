c3ee0ca3245203b1341e88148606fee2d86254e6
David M. Rogers <predictivestatmech@gmail.com>
2020-06-13 14:31:05 -0600

    Fixed ebind and tested ebind,pcm,rrho.

--- autoqct/ebind.py
+++ autoqct/ebind.py
@@ -1,11 +1,16 @@
 from multiprocessing import Process, Pipe
-
-def my_function(x):
-    return x+100
+from tempfile import TemporaryDirectory
+import os
 
 # return counterpoise-corrected binding energy in kcal/mol
 def calcE(conn, chg1, spin1, geom1, chg2, spin2, geom2, theory, basis):
     import psi4
+    olddir = os.getcwd()
+    tmpdir = TemporaryDirectory()
+    os.chdir(tmpdir.name)
+    psi4.core.set_output_file('output.dat', False)
+    psi4.set_memory('100 GB')
+
     psi4.set_options({'freeze_core': 'true'})
     cplx = psi4.geometry("""{chg1} {spin1}
 {geom1}
@@ -13,19 +18,22 @@ def calcE(conn, chg1, spin1, geom1, chg2, spin2, geom2, theory, basis):
 {chg2} {spin2}
 {geom2}
 units angstrom
-""")
+""".format(chg1=chg1, spin1=spin1, geom1=geom1,
+           chg2=chg2, spin2=spin2, geom2=geom2))
     de = psi4.energy('ccsd(t)/aug-cc-pvdz', bsse_type='cp', molecule=cplx)
     conn.send( de * psi4.constants.hartree2kcalmol )
+    os.chdir(olddir)
     conn.close()
 
-def run_ebind(charge1, spin1, crds1, charge2, spin2, crds2, theory, basis):
+def run_ebind(chg1, spin1, crds1, chg2, spin2, crds2, theory, basis):
     recv, send = Pipe(False)
     p = Process(target=calcE, args=(send,
-		chg1, spin1, crds1, chg2, spin2, crds2, theory, basis))
+                chg1, spin1, crds1, chg2, spin2, crds2, theory, basis))
     p.start()
-    ret = recv.recv()
-    ret = p.join() # this blocks until the process terminates
-    return ret
+    de = recv.recv()
+    p.join() # this blocks until the process terminates
+    print("Delta E = %g kcal/mol"%de)
+    return de
 
 def test_run():
     crds1 = "\n".join([
