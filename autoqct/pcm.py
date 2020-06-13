acf6bef9fbbaf3e7a226595f90bde9655ccb3995
David M. Rogers <predictivestatmech@gmail.com>
2020-06-13 14:55:30 -0400

    Added Psi4Run class.

---
+++ autoqct/pcm.py
@@ -0,0 +1,80 @@
+from psi4run import Psi4Run
+
+# Input template
+pcm_in = """#! pcm
+
+molecule mol {{
+{charge} {spin}
+symmetry c1
+{crds}
+units angstrom
+no_reorient
+no_com
+}}
+
+set {{
+  basis {basis}
+  scf_type pk
+  pcm true
+  pcm_scf_type total
+}}
+
+pcm = {{
+    Units = Angstrom
+    Medium {{
+        SolverType = IEFPCM
+        Solvent = Water
+    }}
+
+    Cavity {{
+        RadiiSet = UFF
+        Type = GePol
+        Scaling = false
+        Area = 0.3
+        Mode = Implicit
+    }}
+}}
+
+energy = energy('b3lyp')
+"""
+
+# Example output section:
+"""
+  @RKS Final Energy:   -76.42663379788245
+
+   => Energetics <=
+
+    Nuclear Repulsion Energy =              8.7962296684390751
+    One-Electron Energy =                -122.5818166091975741
+    Two-Electron Energy =                  44.9184051361068342
+    DFT Exchange-Correlation Energy =      -7.5501770580040963
+    Empirical Dispersion Energy =           0.0000000000000000
+    VV10 Nonlocal Energy =                  0.0000000000000000
+    PCM Polarization Energy =              -0.0092749352266872
+    Total Energy =                        -76.4266337978824453
+
+~> gets formatted to {
+     'Nuclear Repulsion': 8.7962296684390751,
+     'One-Electron': -122.5818166091975741,
+     ...
+     'PCM Polarization': -0.0092749352266872,
+     'Total Energy': -76.4266337978824453
+   }
+"""
+
+def run_pcm(crds, charge, spin, basis):
+    p = Psi4Run(pcm_in.format(crds=crds, charge=charge, spin=spin, basis=basis))
+    p.run()
+    if p.err is None:
+        print(p.energies())
+    else:
+        print("Error running psi4")
+
+def test_run():
+    crds = "\n".join([
+                "O  0.000 0.000 0.000",
+                "H  0.803 0.000 0.596",
+                "H -0.803 0.000 0.596"])
+    run_pcm(crds, charge=0, spin=1, basis="cc-pVDZ")
+
+#test_run()
---
+++ autoqct/psi4run.py
@@ -0,0 +1,45 @@
+from tempfile import TemporaryDirectory
+from pathlib import Path
+from subprocess import check_output, CalledProcessError, STDOUT
+import re
+
+class Psi4Run:
+    def __init__(self, inp):
+        self.inp = inp
+        self.tmpdir = TemporaryDirectory()
+        self.dirname = Path( self.tmpdir.name )
+        self.infile = self.dirname / "input.dat"
+        self.err = None
+
+    def run(self):
+        with open(self.infile, "w") as f:
+            f.write(self.inp)
+        try:
+            #self.out = check_output(["cat input.dat"],
+            #             stderr=STDOUT, shell=True,
+            #             cwd=str(self.dirname))
+            self.out = check_output(["psi4"],
+                         stderr=STDOUT,
+                         cwd=str(self.dirname))
+            #print(self.out)
+        except CalledProcessError as e:
+            print("Received error code %d"%e.returncode)
+            print(e.output)
+            self.err = e.returncode
+            self.out = e.output
+
+    def energies(self):
+        attr = {}
+        expr = re.compile(r'\s*(.*)\sEnergy\s*=\s*(.*)')
+        fexpr = re.compile(r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?')
+        with open(self.dirname / "output.dat") as f:
+            for line in f:
+                m = expr.match(line)
+                if m is None:
+                    continue
+                v = fexpr.match(m[2])
+                if v is None:
+                    continue
+                attr[m[1].strip()] = float(v[0])
+        return attr
+
